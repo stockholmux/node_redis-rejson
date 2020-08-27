746219032ed23cf23053c475f7fb7603c5a560f3
Cupcakus Desktop <cupcakus@gmail.com>
2020-08-27 14:04:40 -0400

    Added support for new RedisJSON Commands for queries (JSON.INDEX, JSON.QGET)

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function addReJSONModule(redis) {
-  var cmds = ["json.del", "json.get", "json.mget", "json.set", "json.type", "json.numincrby", "json.nummultby", "json.strappend", "json.strlen", "json.arrappend", "json.arrindex", "json.arrinsert", "json.arrlen", "json.arrpop", "json.arrtrim", "json.objkeys", "json.objlen", "json.debug", "json.forget", "json.resp"];
+  var cmds = ["json.del", "json.get", "json.mget", "json.set", "json.type", "json.numincrby", "json.nummultby", "json.strappend", "json.strlen", "json.arrappend", "json.arrindex", "json.arrinsert", "json.arrlen", "json.arrpop", "json.arrtrim", "json.objkeys", "json.objlen", "json.debug", "json.forget", "json.resp", "json.index", "json.qget"];
   
   cmds.forEach(function(aCmd) {
     redis.addCommand(aCmd);
